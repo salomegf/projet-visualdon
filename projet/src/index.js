c7b671dcd55780ed79d78694fb5f76fa252f81f4
Salomé Gfeller <salome.gfeller@heig-vd.ch>
2022-03-29 14:20:04 +0200

    1st vis

--- projet/src/index.js
+++ projet/src/index.js
@@ -1,10 +1,53 @@
-import * as d3 from 'd3';
-import file from '../assets/International_Astronaut_Database.csv'
+//import * as d3 from 'd3';
+import astronautes from '../assets/International_Astronaut_Database.csv'
 
-console.log(file);
+/*
+const noms = astronautes.map((d,i) => {
+    return d["Total Flights"];
+})
+console.log(noms);*/
 
-const noms = file.map((d,i) => {
-    return d.Name;
+
+const femmes = astronautes.filter((d, i) => {
+    return d.Gender == "Woman"
 })
 
-console.log(noms);
+//SVG
+const body = d3.select("body");
+body.append("svg");
+
+const margin = {
+        top: 10,
+        right: 10,
+        bottom: 10,
+        left: 10
+    },
+    width = 600 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+const svg = d3.select("svg");
+
+svg.attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("class", "group")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+const g = d3.select(".group");
+
+
+
+// 1. Tous les astronautes
+const nbrAstronautes = astronautes.length
+
+g.selectAll("circle")
+    .data(
+        d3.gridding()
+        .size([width, height])
+        .mode("grid")(d3.range(nbrAstronautes))
+    )
+    .enter().append("circle")
+    .attr("r", 5)
+    .attr("transform", function (d) {
+        return "translate(" + d.cx + "," + d.cy + ")";
+    });
